46ac326c6e824be2a13544de03ed657e467c0f9f
tejaswiltts <78849214+tejaswiltts@users.noreply.github.com>
2021-02-26 16:57:03 +0530

    Update InningsDetails.java

--- InningsDetails.java
+++ InningsDetails.java
@@ -1,42 +1,41 @@
-class InningsDetails
-{
+public class Innings {
+	String end="second";
+	String teamname;
+	public String getTeamname() {
+		return teamname;
+	}
+	public void setTeamname(String teamname) {
+		this.teamname = teamname;
+	}
+	public String getInningsname() {
+		return inningsname;
+	}
+	public void setInningsname(String inningsname) {
+		this.inningsname = inningsname;
+	}
+	public int getRuns() {
+		return runs;
+	}
+	public void setRuns(int runs) {
+		this.runs = runs;
+	}
+	String inningsname;
+	int runs;
+	
 
-private String teamname;
-private String inningsname;
-private int runs;
-public void setTeamname(String teamname)
-{
-this.teamname=teamname;
-}
+	public void displayInningsDetails(int run2, String ses) {
+		if(inningsname.equals(end))
+		{
+			System.out.println("Match Ended");
+		}
+		else
+		{
+			int runNeed=run2+1;
+			System.out.println("Needs "+runNeed+" to win");
+		}
+		
+	}
+	
+	
 
-public void setInningsname(String inningsname)
-{
-this.inningsname=inningsname;
-}
-public void setRuns(int runs)
-{
-this.runs=runs;
-}
-
-public String getTeamname(){
-return teamname;
-}
-public String getInningsname(){
-return Innings;
-}
-public int getRuns(){
-return runs;
-}
-
-displayInningsDetails(int runs) 
-{
-int runNeed=runs+1;
-System.out.println(+runNeed"to win");
-}
-public void displayDetails(){
-
-System.out.println("Teamname"+teamname);
-System.out.println("Inningsname:"+inningsname);
-System.out.println("Runs:"+runs);
-}
 }
