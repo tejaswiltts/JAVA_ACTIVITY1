99d7cdd7958eed31d6d7636ed4aec3a4c947b772
tejaswiltts <78849214+tejaswiltts@users.noreply.github.com>
2021-02-26 17:25:58 +0530

    Create Employee9.java

---
+++ Employee9.java
@@ -0,0 +1,31 @@
+public class Employee5 {
+	String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	String address;
+	String mobile;
+
+}
