282a6bcbe4330987d2dd0edf4cd27a27afc0c18b
tejaswiltts <78849214+tejaswiltts@users.noreply.github.com>
2021-02-27 10:05:12 +0530

    Create TwoWheeler.java

---
+++ TwoWheeler.java
@@ -0,0 +1,18 @@
+package tejaswi;
+
+public class TwoWheeler extends Vehicle {
+	private boolean kickStartAvailable;
+	
+	public boolean isKickStartAvailable() {
+		return kickStartAvailable;
+	}
+
+	public void setKickStartAvailable(boolean kickStartAvailable) {
+		this.kickStartAvailable = kickStartAvailable;
+	}
+
+	static void displayDetailInfo()
+	{
+		
+	}
+}
