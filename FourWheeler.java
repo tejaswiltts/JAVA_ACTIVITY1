27a940994b9c92254e7e91241334ad009c5e50ae
tejaswiltts <78849214+tejaswiltts@users.noreply.github.com>
2021-02-27 10:06:32 +0530

    Create FourWheeler.java

---
+++ FourWheeler.java
@@ -0,0 +1,26 @@
+package tejaswi;
+
+public class FourWheeler extends Vehicle
+{
+	private String AudioSystem ;
+	private String NumberOfDoors; 
+	
+	
+	public String getAudioSystem() {
+		return AudioSystem;
+	}
+	public void setAudioSystem(String AudioSystem) {
+		this.AudioSystem = AudioSystem;
+	}
+	public String getNumberOfDoors() {
+		return NumberOfDoors;
+	}
+	public void setNumberOfDoors(String NumberOfDoors) {
+		this.NumberOfDoors = NumberOfDoors;
+	}
+	
+static void displayDetailInfo()
+{
+	
+}
+}
